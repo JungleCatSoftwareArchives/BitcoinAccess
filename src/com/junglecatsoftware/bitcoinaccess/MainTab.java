88e67a09a730e77a7d3a581e8126d086ec2785ba
Bryan Wyatt <brwyatt@gmail.com>
2013-03-11 23:25:06 -0500

    remove processing from MainTab class

--- src/com/junglecatsoftware/bitcoinaccess/MainTab.java
+++ src/com/junglecatsoftware/bitcoinaccess/MainTab.java
@@ -19,12 +19,15 @@
 
 package com.junglecatsoftware.bitcoinaccess;
 
+import com.junglecatsoftware.bitcoinaccess.BitcoinRPC.GetBalance;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainTab extends Fragment {
 	
@@ -36,37 +39,9 @@ public class MainTab extends Fragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		final ViewGroup root = (ViewGroup) inflater.inflate(R.layout.main_tab, null);
+		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.main_tab, null);
 		
-		root.findViewById(R.id.Main_Balance_Label).setVisibility(View.INVISIBLE);
-		root.findViewById(R.id.Main_Balance_Value).setVisibility(View.INVISIBLE);
-		root.findViewById(R.id.Main_Unconfirmed_Label).setVisibility(View.INVISIBLE);
-		root.findViewById(R.id.Main_Unconfirmed_Value).setVisibility(View.INVISIBLE);
-		root.findViewById(R.id.Main_Send_Button).setVisibility(View.INVISIBLE);
-		root.findViewById(R.id.Main_Rec_Button).setVisibility(View.INVISIBLE);
-		root.findViewById(R.id.Main_Spinner).setVisibility(View.VISIBLE);
-		
-		//Data Fetch
-		new Thread(new Runnable() {
-			public void run(){
-				//Artificial Delay (for now)
-				try{
-					Thread.sleep(5000);
-				}catch(Exception e){
-				}
-				root.post(new Runnable(){
-					public void run(){
-						root.findViewById(R.id.Main_Balance_Label).setVisibility(View.VISIBLE);
-						root.findViewById(R.id.Main_Balance_Value).setVisibility(View.VISIBLE);
-						root.findViewById(R.id.Main_Unconfirmed_Label).setVisibility(View.VISIBLE);
-						root.findViewById(R.id.Main_Unconfirmed_Value).setVisibility(View.VISIBLE);
-						root.findViewById(R.id.Main_Send_Button).setVisibility(View.VISIBLE);
-						root.findViewById(R.id.Main_Rec_Button).setVisibility(View.VISIBLE);
-						root.findViewById(R.id.Main_Spinner).setVisibility(View.GONE);
-					}
-				});
-			}
-		}).start();
+		new GetBalance().execute((TextView)root.findViewById(R.id.Main_Balance_Value));
 		
 		return root;
 	}
