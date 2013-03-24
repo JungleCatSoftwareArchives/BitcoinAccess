ea116513c3ad0891a500a09b6963ce404c4de2e6
Bryan Wyatt <brwyatt@gmail.com>
2013-03-23 20:28:16 -0500

    Add task for retrieving unconfirmed (0-conf) balance

---
+++ src/com/junglecatsoftware/bitcoinaccess/BitcoinRPC/GetUnconfirmedBalance.java
@@ -0,0 +1,58 @@
+package com.junglecatsoftware.bitcoinaccess.BitcoinRPC;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GetUnconfirmedBalance extends AsyncTask<TextView,Integer,String> {
+	private TextView[] textViews;
+	private Context context = null;
+	
+	public GetUnconfirmedBalance(){
+		super();
+	}
+	public GetUnconfirmedBalance(Context context){
+		super();
+		this.context = context;
+	}
+	
+	@Override
+	protected String doInBackground(TextView... textViews) {
+		this.textViews = textViews;
+		if(context == null){
+			context = textViews[0].getContext().getApplicationContext();
+		}
+		
+		return BitcoinRPC.getBalance(context,0);
+	}
+	
+	@SuppressLint("DefaultLocale")
+	@Override
+	protected void onPostExecute(String result) {
+		String balance = "--";
+		if(result.toLowerCase().startsWith("error")){
+			if(context!=null){
+				Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+			}
+		}else{
+			try {
+				JSONObject json = new JSONObject(result);
+				
+				balance = json.getString("result");
+			} catch (JSONException e) {
+				if(context!=null){
+					Toast.makeText(context, "ERROR: Unable to parse response from server", Toast.LENGTH_LONG).show();
+				}
+			}
+		}
+		
+		for(TextView textView : textViews){
+			textView.setText(balance);
+		}
+	}
+}
